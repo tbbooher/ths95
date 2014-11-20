8732800ef4d8b0108b844b235f6aff07f89ca0db
Tim Booher <tim@theboohers.org>
2014-11-20 11:25:13 -0500

    getting things working

--- header.php
+++ header.php
@@ -16,9 +16,11 @@
 
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    
 <title>
 <?php wp_title('|', true, 'right'); ?>
 </title>
+    
 <meta name="viewport" content="width=device-width">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
