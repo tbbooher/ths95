20a601e79f0f2217c92a92ddfe64fff2818ffd18
Tim Booher <tim@theboohers.org>
2014-11-21 05:33:23 -0500

    updated templates

--- page.php
+++ page.php
@@ -10,10 +10,9 @@ get_header();
 /**
  * determine main column size from actived sidebar
  */
-$main_column_size = bootstrapBasicGetMainColumnSize();
+//$main_column_size = bootstrapBasicGetMainColumnSize();
 ?> 
-<?php get_sidebar('left'); ?> 
-				<div class="col-md-<?php echo $main_column_size; ?> content-area" id="main-column">
+				<div class="col-md-8 content-area" id="main-column">
 					<main id="main" class="site-main" role="main">
 						<?php 
 						while (have_posts()) {
@@ -35,5 +34,33 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 						?> 
 					</main>
 				</div>
-<?php get_sidebar('right'); ?> 
+                <div class="col-md-4 content-area" id="main-column">
+                    <h3>Upcoming Events</h3>
+                    <div style="background-color: orange">
+                    <?php
+                    $args = array( 'post_type' => 'news_item', 'posts_per_page' => 1 );
+                    $loop = new WP_Query( $args );
+                    while ( $loop->have_posts() ) : $loop->the_post();
+                      the_title( '<h4>', '</h4>' );
+                      echo '<div class="entry-content">';
+                      the_content();
+                      echo '</div>';
+                    endwhile;
+                    ?>    
+                    </div>
+                    <div style="background-color: gray">
+                    <h3>Latest News</h3>
+                    <?php
+                    $args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
+                    $loop = new WP_Query( $args );
+                    while ( $loop->have_posts() ) : $loop->the_post();
+                      the_title( '<h4>', '</h4>' );
+                      echo '<div class="entry-content">';
+                      the_content();
+                      echo '</div>';
+                    endwhile;
+                    ?>    
+                    </div>
+                </div>
+
 <?php get_footer(); ?> 
