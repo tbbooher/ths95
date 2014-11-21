2ddcbbed0d4bfe5946f06bdb1ab770eb70ad1f68
Tim Booher <tim@theboohers.org>
2014-11-21 04:11:20 -0500

    troubleshooting

--- page-home.php
+++ page-home.php
@@ -9,8 +9,8 @@ get_header();
 
 ?> 
 
-<!-- end test -->
-
+            <!-- see http://bootstrapbay.com/blog/bootstrap-3-carousel-tutorial/ -->
+    
             <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
                   <!-- Indicators -->
                   <ol class="carousel-indicators">
@@ -23,9 +23,9 @@ get_header();
                       <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
                       <div class="container">
                         <div class="carousel-caption">
-                          <h1>Example headline.</h1>
-                          <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-                          <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                          <h1>Cool Picture and Content</h1>
+                          <p>This is where awesome happens!</p>
+                          <p><a class="btn btn-lg btn-primary" href="/getting-started/" role="button">Sign up today</a></p>
                         </div>
                       </div>
                     </div>
@@ -35,7 +35,7 @@ get_header();
                         <div class="carousel-caption">
                           <h1>Another example headline.</h1>
                           <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                          <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                          <p><a class="btn btn-lg btn-primary" href="/getting-started/" role="button">Learn more</a></p>
                         </div>
                       </div>
                     </div>
@@ -45,7 +45,7 @@ get_header();
                         <div class="carousel-caption">
                           <h1>One more for good measure.</h1>
                           <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                          <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                          <p><a class="btn btn-lg btn-primary" href="/getting-started/" role="button">Browse gallery</a></p>
                         </div>
                       </div>
                     </div>
