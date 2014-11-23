095305e6f251c02b16afeaafaa760855d7a1be52
Tim Booher <tim@theboohers.org>
2014-11-23 07:31:42 -0500

    unhide slider

--- page-home.php
+++ page-home.php
@@ -10,7 +10,6 @@ get_header();
 ?> 
 
             <!-- see http://bootstrapbay.com/blog/bootstrap-3-carousel-tutorial/ -->
-            <?php /* 
             <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
                   <!-- Indicators -->
                   <ol class="carousel-indicators">
@@ -59,7 +58,6 @@ get_header();
                     <span class="sr-only">Next</span>
                   </a>
                 </div>
-                */ ?>
                 <!-- end carousel -->
 				<div class="container">
                     <div class="row">
