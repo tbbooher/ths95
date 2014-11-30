d75cff4153b9a3cb13b2d87d3253d91532aaf426
Derick Montague <derick@beyondhyper.com>
2014-11-29 20:27:57 -0600

    Updated templates to have the same column layout

--- page-our-programs.php
+++ page-our-programs.php
@@ -12,7 +12,7 @@ get_header();
  */
 //$main_column_size = bootstrapBasicGetMainColumnSize();
 ?> 
-				<div class="col-md-8 content-area" id="main-column">
+				<div class="col-md-9 content-area" id="main-column">
 					<main id="main" class="site-main" role="main">
 						<?php 
 						while (have_posts()) {
@@ -62,7 +62,7 @@ get_header();
 						?> 
 					</main>                    
 				</div>
-                <div class="col-md-4 content-area" id="main-column">
+                <aside class="col-md-3 content-area" id="main-column">
                     <div class="the_image">
                         <?php the_post_thumbnail('medium'); ?>
                     </div>
@@ -72,26 +72,26 @@ get_header();
                         $args = array( 'post_type' => 'news_item', 'posts_per_page' => 1 );
                         $loop = new WP_Query( $args );
                         while ( $loop->have_posts() ) : $loop->the_post();
-                          the_title( '<h4>', '</h4>' );
-                          echo '<div class="entry-content">';
-                          the_content();
-                          echo '</div>';
+                        the_title( '<h4>', '</h4>' );
+                        echo '<div class="entry-content">';
+                        the_content();
+                        echo '</div>';
                         endwhile;
                         ?>    
                     </div>
+                    <h3>Latest News</h3>
                     <div class="content-box big ch-item bottom-pad-small">
-                        <h3>Latest News</h3>
                         <?php
-                        $args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
-                        $loop = new WP_Query( $args );
-                        while ( $loop->have_posts() ) : $loop->the_post();
-                          the_title( '<h4>', '</h4>' );
-                          echo '<div class="entry-content">';
-                          the_content();
-                          echo '</div>';
-                        endwhile;
+                            $args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
+                            $loop = new WP_Query( $args );
+                            while ( $loop->have_posts() ) : $loop->the_post();
+                                the_title( '<h4>', '</h4>' );
+                                echo '<div class="entry-content">';
+                                the_content();
+                                echo '</div>';
+                            endwhile;
                         ?>    
                     </div>
-                </div>
+                </aside>
 
 <?php get_footer(); ?> 
--- page.php
+++ page.php
@@ -44,20 +44,20 @@ get_header();
 			$args = array( 'post_type' => 'news_item', 'posts_per_page' => 1 );
 			$loop = new WP_Query( $args );
 			while ( $loop->have_posts() ) : $loop->the_post();
-			the_title( '<h3>', '</h3>' );
+			the_title( '<h4>', '</h4>' );
 			echo '<div class="entry-content">';
 			the_content();
 			echo '</div>';
 			endwhile;
 			?>    
 		</div>
+		<h3>Latest News</h3>
 		<div class="content-box big ch-item bottom-pad-small">
-			<h3>Latest News</h3>
 			<?php
 				$args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
 				$loop = new WP_Query( $args );
 				while ( $loop->have_posts() ) : $loop->the_post();
-					the_title( '<h3>', '</h3>' );
+					the_title( '<h4>', '</h4>' );
 					echo '<div class="entry-content">';
 					the_content();
 					echo '</div>';
