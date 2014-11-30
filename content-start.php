d266f16d5fe61264ebf9f63aff5e0dd78a251c4b
Derick Montague <derick@beyondhyper.com>
2014-11-30 01:05:58 -0600

    Added Pricing

--- content-start.php
+++ content-start.php
@@ -6,17 +6,19 @@
 	<div class="entry-content">
 		<?php the_content(); ?> 
         <!-- custom getting started content -->
+            <h2>Pricing</h2>
             <div role="tabpanel">
 
               <!-- Nav tabs -->
               <ul class="nav nav-tabs" role="tablist" id="getting_started_tabs">
                 <li role="presentation" class="active"><a href="#elements" aria-controls="elements" role="tab" data-toggle="tab">Elements</a></li>
                 <li role="presentation"><a href="#membership" aria-controls="membership" role="tab" data-toggle="tab">Membership</a></li>
+                <li role="presentation"><a href="#barbell_club" aria-controls="barbell_club" role="tab" data-toggle="tab">Barbell Club</a></li>
                 <li role="presentation"><a href="#skills_test" aria-controls="skills_test" role="tab" data-toggle="tab">Skills Test/Drop In</a></li>
               </ul>
 
               <!-- Tab panes -->
-              <div class="tab-content">
+              <div class="pricing tab-content">
                 <div role="tabpanel" class="tab-pane fade in active" id="elements">
                   <p>
                       These classes last about one hour and are made up of two to three
@@ -28,13 +30,102 @@
                       Mandatory completion is required, before signing up for a membership.
                       These classes teach movements for optimal safety and performance
                       in our program.
-                  </p>                  
+                  </p>
+                  <div class="row">
+                    <div class="col-md-4">
+                      <h3 class="pricing-desc">Large Group</h3>
+                      <h4 class="pricing-cost special"><small>$</small>190</h4>
+                      <ul class="pricing-list">
+                        <li>Group Setting</li>
+                        <li>8 total classes</li>
+                        <li>2X a week for 1 month</li>
+                      </ul>
+                    </div>
+                    <div class="col-md-4">
+                      <h3 class="pricing-desc">Small Group</h3>
+                      <h4 class="pricing-cost"><small>$</small>330</h4>
+                      <ul class="pricing-list">
+                        <li>Small Group Setting (2-3) participants</li>
+                        <li>8 total classes</li>
+                        <li>2X a week for 1 month</li>
+                      </ul>
+                    </div>
+                    <div class="col-md-4">
+                      <h3 class="pricing-desc">Private</h3>
+                      <h4 class="pricing-cost"><small>$</small>450</h4>
+                      <ul class="pricing-list">
+                        <li>Individual One on One</li>
+                        <li>8 total classes</li>
+                        <li>2X a week for 1 month</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <h5 class="pricing-special">15% discount offered to Law Enforcement Officers (LEO), Active Military (MIL), Firefighters (FF), and Full-time Teachers (FTT).</h5>
+                  <h5><strong>IMPORTANT:</strong> Upon completion of Elements, you will be automatically signed up for an unlimited membership for the following month! If you would rather sign up for 9x or 13x a month, you must <a href="mailto:lisa@crossfitadaptation.com">email lisa@crossfitadaptation.com</a>.</h5>
                 </div>
                 <div role="tabpanel" class="tab-pane fade " id="membership">
-                  <p>derick please update membership info</p>
+                   <div class="row">
+                    <div class="col-md-3">
+                      <h3 class="pricing-cost"><small>$</small>160/month</h3>
+                      <ul class="pricing-list">
+                        <li>9x WODs</li>
+                        <li>Unlimited Yoga</li>
+                        <li>Unlimited Olympic Lifting Workshop</li>
+                      </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <h3 class="pricing-cost"><small>$</small>180/month</h3>
+                      <ul class="pricing-list">
+                        <li>10 class punch cards</li>
+                        <li>WODs Only</li>
+                      </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <h3 class="pricing-cost"><small>$</small>195/month</h3>
+                      <ul class="pricing-list">
+                        <li>13x WODs</li>
+                        <li>Unlimited Yoga</li>
+                        <li>Unlimited Olympic Lifting Workshop</li>
+                      </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <h3 class="pricing-cost"><small>$</small>220/month</h3>
+                      <ul class="pricing-list">
+                        <li>Unlimited WODs</li>
+                        <li>Unlimited Yoga</li>
+                        <li>Unlimited Olympic Lifting Workshop</li>
+                      </ul>
+                    </div>                    
+                  </div>
+                  <h4>Discounts</h4>
+                  <ul>
+                    <li>15% off – Sworn Law Enforcement Officers, Active Military, Fire Fighters, and Full-time Teachers</li>
+                    <li>10% off – Married couple memberships</li>
+                  </ul>
+                </div>
+                <div role="tabpanel" class="tab-pane fade" id="barbell_club">
+                  <p>The Adaptation Barbell Club meets at CFA every Tuesday and Thursday at 7:30 PM, and Sundays at 11:30 AM. Rick runs every Sunday and Tuesday workouts. If you’re interested in taking your Olympic lifts beyond the Sunday workshop, this is your opportunity.</p>
+                  <p>The focused lifts are the clean and jerk and the snatch. A percentage based program is utilized with a multitude of other lifts, to include the romanian deadlift, snatch pulls, and back squats, just to name a few. These sessions generally last one hour.</p>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <h3 class="pricing-desc">Current CFA Members</h3>
+                      <h4 class="pricing-cost"><small>$</small>80 for 3 months</h4>
+                    </div>
+                    <div class="col-md-6">
+                      <h3 class="pricing-desc">Non-CFA members</h3>
+                      <h4 class="pricing-cost"><small>$</small>250 for 3 months</h4>
+                    </div>
+                  </div>
+                  <h5>If you are interested in joining or have any questions then <a href="mailto:info@crossfitadaptation.com">email info@crossfitadaptation.com</a>.</h5>
                 </div>
                 <div role="tabpanel" class="tab-pane fade" id="skills_test">
-                  <p>derick please update skills test information</p>
+                  <h3>Skills Test-in</h3>
+                  <p>If you are new to our affiliate, this option allows you to test-in for membership. The test requires you to effectively demonstrate a variety of CrossFit skills with proper mechanics. This is typically reserved for those that have trained extensively at another CrossFit affiliate.</p>
+                  <h4>Test-In Fee: $15</h4>
+                  <p>After successful completion of the test-in, membership is offered and you can sign-up. If interested, just <a href="info@crossfitadaptation.com">email us at info@crossfitadaptation.com</a>.</p>
+                  <h3>Drop-ins</h3>
+                  <h4>Drop-In fee: $20</h4>
+                  <h4>Drop-In fee w/ shirt or tank: $30</h4>
                 </div>
               </div>
 
