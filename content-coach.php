2c22c5c7574427aeeb8cccc8cdf7bce3ba9f9322
tim@nitrous.io <tim@theboohers.org>
2014-12-01 15:11:40 +0000

    updated content-coach again

--- content-coach.php
+++ content-coach.php
@@ -5,8 +5,12 @@
                  <?php the_post_thumbnail('coach-thumb'); ?>
             </div>
                <div class="col-md-7 coachName">
-                 	<h2 class="entry-title">      <?php the_title(); ?>    </h2>
-                    Email <a href="<?php get_post_meta(get_the_ID(), 'email', true) ?>"><?php the_title(); ?></a>
+                 	<h2 class="entry-title">
+                    foo!
+                    <?php the_title(); ?>
+                  </h2>
+                    Email <a href="<?php get_post_meta(get_the_ID(), 'email', true) ?>">
+                 <?php the_title(); ?></a>
         <?php bootstrapBasicEditPostLink(); ?>
        
               </div>
