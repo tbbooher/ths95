a715a80aa44b4adb2599dd285b4cc10aa884e191
tim@nitrous.io <tim@theboohers.org>
2014-12-01 14:35:08 +0000

    new coach

--- content-coach.php
+++ content-coach.php
@@ -1,17 +1,21 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+- <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h2 class="entry-title"><?php the_title(); ?></h2>
+	</header><!-- .entry-header -->
 
-	<div class="entry-content col-md-6 coach">
-            <div class=" col-md-5">
-                 <?php the_post_thumbnail('medium'); ?>
-            </div>
-               <div class="col-md-7 coachName">
-                 	<h2 class="entry-title">      <?php the_title(); ?>    </h2>
-                    Email <a href="<?php get_post_meta(get_the_ID(), 'email', true) ?>"><?php the_title(); ?></a>
-        <?php bootstrapBasicEditPostLink(); ?>
-       
-              </div>
+  <!-- this is what needs to be updated with the new styles you wrote 
+       and this is the file that should be updated . . . -->
+	<div class="entry-content">
+        <div class="the_image">
+            <?php set_post_thumbnail_size( 50, 50, true ); ?>
+            <?php the_post_thumbnail('medium'); ?>
+        </div>
+		<?php the_content(bootstrapBasicMoreLinkText()); ?> 
 		<div class="clearfix"></div>
-           <?php the_content(bootstrapBasicMoreLinkText()); ?> 
 	</div><!-- .entry-content -->
-
+	
+	<footer class="entry-meta">
+        Email <a href="<?php get_post_meta(get_the_ID(), 'email', true) ?>"><?php the_title(); ?></a>
+        <?php bootstrapBasicEditPostLink(); ?>
+	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
--- page-coaches.php
+++ page-coaches.php
@@ -44,12 +44,13 @@ get_header();
                         ?>
                     </div>
 				</div>
-
-<div class="col-md-3 content-area" id="main-column">
-                    <h3>Upcoming Events</h3>
-                    
-                        <div class="entry-content">
-                           <?php
+        <aside class="col-md-3 content-area" id="main-column">
+          <div class="the_image">
+            <?php the_post_thumbnail('medium'); ?>
+          </div>
+          <h3>Upcoming Events</h3>
+          <div class="content-box big ch-item bottom-pad-small">
+            <?php
             $args = array( 'post_type' => 'news_item', 'posts_per_page' => 1 );
             $loop = new WP_Query( $args );
             while ( $loop->have_posts() ) : $loop->the_post();
@@ -59,23 +60,20 @@ get_header();
             echo '</div>';
             endwhile;
             ?>    
-                          </div>  
-                      <h3>Recent News</h3>      
-                         <?php the_post_thumbnail('medium'); ?> <br>
-                        <p>Now available in fridge!</p>
-                        <div class="entry-content">
-                          <?php
-                            $args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
-                            $loop = new WP_Query( $args );
-                            while ( $loop->have_posts() ) : $loop->the_post();
-                              the_title( '<h4>', '</h4>' );
-                              echo '<div class="entry-content">';
-                              the_content();
-                              echo '</div>';
-                            endwhile;
-                          ?>
-                          </div> 
-                    </div>
-
+          </div>
+          <h3>Latest News</h3>
+          <div class="content-box big ch-item bottom-pad-small">
+            <?php
+              $args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
+              $loop = new WP_Query( $args );
+              while ( $loop->have_posts() ) : $loop->the_post();
+                the_title( '<h4>', '</h4>' );
+                echo '<div class="entry-content">';
+                the_content();
+                echo '</div>';
+              endwhile;
+            ?>    
+          </div>
+        </aside>
 
 <?php get_footer(); ?> 
