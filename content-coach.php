215bcea1f214525eb0993bab3cb783cabc785953
tim@nitrous.io <tim@theboohers.org>
2014-12-01 14:37:32 +0000

    updated content coach

--- content-coach.php
+++ content-coach.php
@@ -1,21 +1,26 @@
-- <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h2 class="entry-title"><?php the_title(); ?></h2>
+- <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> class="col-md-6 coach">
+  
+  <div class="the_image img-responsive col-md-5">
+    <!-- are you here?? -->
+    <?php the_post_thumbnail('medium'); ?>
+  </div>
+	<header class="entry-header col-md-7 coachName">
+		<h2 class="entry-title">
+      <?php the_title(); ?>
+    </h2>
+    <!-- and you can see this, correct? -->
+    <h3>
+      <?php get_post_meta(get_the_ID(), 'title', true) ?>
+    </h3>
 	</header><!-- .entry-header -->
-
-  <!-- this is what needs to be updated with the new styles you wrote 
-       and this is the file that should be updated . . . -->
-	<div class="entry-content">
-        <div class="the_image">
-            <?php set_post_thumbnail_size( 50, 50, true ); ?>
-            <?php the_post_thumbnail('medium'); ?>
-        </div>
+  <div class="clearfix"></div>
+	<div class="col-md-12">
 		<?php the_content(bootstrapBasicMoreLinkText()); ?> 
 		<div class="clearfix"></div>
+    <footer class="entry-meta">
+      Email <a href="<?php get_post_meta(get_the_ID(), 'email', true) ?>"><?php the_title(); ?></a>
+      <?php bootstrapBasicEditPostLink(); ?>
+    </footer><!-- .entry-meta -->
 	</div><!-- .entry-content -->
 	
-	<footer class="entry-meta">
-        Email <a href="<?php get_post_meta(get_the_ID(), 'email', true) ?>"><?php the_title(); ?></a>
-        <?php bootstrapBasicEditPostLink(); ?>
-	</footer><!-- .entry-meta -->
-</article><!-- #post-## -->
+</article><!-- #post-## -->\
