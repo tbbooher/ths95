7ec9bd3a4f497b0163f95788497a5cb77f9618c2
Tim Booher <tim@theboohers.org>
2014-12-02 09:08:14 -0500

    updated

--- archive-wod.php
+++ archive-wod.php
@@ -52,5 +52,36 @@ get_header();
 						<?php } //endif; ?> 
 					</main>
 				</div>
+                <aside class="col-md-4 content-area" id="main-column">
+                    <h3>Community Stuff</h3>
+                    <p>Flickr Pictures</p>
+                    <p>YouTube Videos</p>
+                    <h3>Upcoming Events</h3>
+                    <div class="content-box big ch-item bottom-pad-small well">
+                        <?php
+                        $args = array( 'post_type' => 'news_item', 'posts_per_page' => 1 );
+                        $loop = new WP_Query( $args );
+                        while ( $loop->have_posts() ) : $loop->the_post();
+                        the_title( '<h4>', '</h4>' );
+                        echo '<div class="entry-content">';
+                        the_content();
+                        echo '</div>';
+                        endwhile;
+                        ?>    
+                    </div>
+                    <h3>Latest News</h3>
+                    <div class="content-box big ch-item bottom-pad-small well">
+                        <?php
+                            $args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
+                            $loop = new WP_Query( $args );
+                            while ( $loop->have_posts() ) : $loop->the_post();
+                                the_title( '<h4>', '</h4>' );
+                                echo '<div class="entry-content">';
+                                the_content();
+                                echo '</div>';
+                            endwhile;
+                        ?>    
+                    </div>
+                </aside>
 <?php get_sidebar('right'); ?> 
 <?php get_footer(); ?> 
