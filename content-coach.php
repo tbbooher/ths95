033517b5d655ae9421d31a63630c986cbd201e0f
Tim Booher <tim@theboohers.org>
2014-12-02 19:48:05 -0500

    playing with coach format

--- content-coach.php
+++ content-coach.php
@@ -1,15 +1,21 @@
 
-  <div  id="post-<?php the_ID(); ?>" <?php post_class('col-md-6 col-sm-6'); ?>>
-            <div class="row col-md-5">
-                 <?php the_post_thumbnail('medium'); ?>
-            </div>
-               <div class="col-md-7 coachName">
-                  <h2 class="entry-title">      <?php the_title(); ?>    </h2>
-                    Email <a href="<?php get_post_meta(get_the_ID(), 'email', true) ?>"><?php the_title(); ?></a>
-                    <?php bootstrapBasicEditPostLink(); ?>
-       
-              </div>
+  <div  id="post-<?php the_ID(); ?>" <?php post_class('coach_float'); ?>>
+    <div class="row col-md-5">
+         <?php the_post_thumbnail('medium', array('class' => 'img-rounded')); ?>
+    </div>
+    <div class="col-md-7 coachName">
+      <h2 class="entry-title">
+          <?php the_title(); ?>
+      </h2>
+      <div class="email">
+        Email <a href="<?php get_post_meta(get_the_ID(), 'email', true) ?>"><?php the_title(); ?></a>                    
+        <?php bootstrapBasicEditPostLink(); ?>
+      </div>
+    </div>                                                                                       
     <div class="clearfix"></div>
-   
-           <?php the_content(bootstrapBasicMoreLinkText()); ?> 
+    <div class="coach_desc text-justify">
+        <hr>                                            
+        <?php the_content(bootstrapBasicMoreLinkText()); ?> 
+        <hr>
+    </div>
   </div>
--- page-coaches.php
+++ page-coaches.php
@@ -13,82 +13,62 @@ get_header();
 //$main_column_size = bootstrapBasicGetMainColumnSize();
 ?> 
         <div class="row col-md-9 content-area" id="main-column">
-        <div class="col-md-12 coachtitle">
-            <?php 
-            while (have_posts()) {
-              the_post();
+            <div class="col-md-12 coachtitle">
+                <?php 
+                while (have_posts()) {
+                  the_post();
 
-              get_template_part('content', 'page');
+                  get_template_part('content', 'page');
 
-              echo "\n\n";
-              
-              // If comments are open or we have at least one comment, load up the comment template
-                            // we don't think we will every have comments on a static page
-              if (comments_open() || '0' != get_comments_number()) {
-                comments_template();
-              }
-
-              echo "\n\n";
-
-            } //endwhile;
-            ?> 
-        </div>  
+                } //endwhile;
+                ?> 
+            </div>  
                     <!-- now display coaches -->
                     
-<?php
-$i = 1;
- $args = array( 'post_type' => 'coach', 'posts_per_page' => 20 );
- $loop = new WP_Query( $args );
-
-//added before to ensure it gets opened
-echo '<div class="clearfix">';
- while ( $loop->have_posts() ) : $loop->the_post();
- get_template_part('content', 'coach');
-     // post stuff...
-
-     // if multiple of 3 close div and open a new div
-     if($i % 2 == 0) {echo '</div><div class="clearfix">';}
-
-$i++; endwhile;
-//make sure open div is closed
-echo '</div>';
-?>
-
-                    
-                  
+            <?php
+                $i = 1;
+                $args = array( 'post_type' => 'coach', 'posts_per_page' => 20 );
+                $loop = new WP_Query( $args );
+                //added before to ensure it gets opened
+                echo '<div class="clearfix">';
+                while ( $loop->have_posts() ) : $loop->the_post();
+                    get_template_part('content', 'coach');
+                     // if multiple of 3 close div and open a new div
+                     if($i % 2 == 0) {echo '</div><div class="clearfix">';}
+                    $i++;
+                endwhile;
+                //make sure open div is closed
+                echo '</div>';
+            ?>                                  
         </div>
 
-<div class="col-md-3 content-area" id="main-column">
-                    <h3>Upcoming Events</h3>
-                    
-                        <div class="entry-content">
-                           <?php
-            $args = array( 'post_type' => 'news_item', 'posts_per_page' => 1 );
-            $loop = new WP_Query( $args );
-            while ( $loop->have_posts() ) : $loop->the_post();
-            the_title( '<h4>', '</h4>' );
-            echo '<div class="entry-content">';
-            the_content();
-            echo '</div>';
-            endwhile;
-            ?>    
-                          </div>  
-                      <h3>Recent News</h3>      
-                         <?php the_post_thumbnail('medium'); ?> <br>
-                        <p>Now available in fridge!</p>
-                        <div class="entry-content">
-                          <?php
-                            $args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
-                            $loop = new WP_Query( $args );
-                            while ( $loop->have_posts() ) : $loop->the_post();
-                              the_title( '<h4>', '</h4>' );
-                              echo '<div class="entry-content">';
-                              the_content();
-                              echo '</div>';
-                            endwhile;
-                          ?>
-                          </div> 
-                    </div>
+        <div class="col-md-3 content-area" id="main-column">
+            <h3>Upcoming Events</h3>
+            <div class="entry-content well">
+                <?php
+                    $args = array( 'post_type' => 'news_item', 'posts_per_page' => 1 );
+                    $loop = new WP_Query( $args );
+                    while ( $loop->have_posts() ) : $loop->the_post();
+                        the_title( '<h4>', '</h4>' );
+                        the_content();
+                    endwhile;
+                ?>    
+            </div>  
+            <h3>Recent News</h3>      
+            <?php the_post_thumbnail('medium'); ?> <br>
+            <div class="entry-content well">
+              <?php
+                $args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
+                $loop = new WP_Query( $args );
+                while ( $loop->have_posts() ) : $loop->the_post();
+                  the_title( '<h4>', '</h4>' );
+                  echo '<div class="entry-content">';
+                  the_content();
+                  echo '</div>';
+                endwhile;
+              ?>
+            </div> 
+        </div>
 
 
 <?php get_footer(); ?> 
