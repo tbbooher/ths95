5bee83dfbbbe8691f6b5a85f7aac80d47220e708
Tim Booher <tim@theboohers.org>
2014-12-03 20:48:00 -1000

    from_derick.css is back

--- page-coaches.php
+++ page-coaches.php
@@ -43,31 +43,7 @@ get_header();
         </div>
 
         <div class="col-md-3 content-area" id="main-column">
-            <h3>Upcoming Events</h3>
-            <div class="entry-content well">
-                <?php
-                    $args = array( 'post_type' => 'news_item', 'posts_per_page' => 1 );
-                    $loop = new WP_Query( $args );
-                    while ( $loop->have_posts() ) : $loop->the_post();
-                        the_title( '<h4>', '</h4>' );
-                        the_content();
-                    endwhile;
-                ?>    
-            </div>  
-            <h3>Recent News</h3>      
-            <?php the_post_thumbnail('medium'); ?> <br>
-            <div class="entry-content well">
-              <?php
-                $args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
-                $loop = new WP_Query( $args );
-                while ( $loop->have_posts() ) : $loop->the_post();
-                  the_title( '<h4>', '</h4>' );
-                  echo '<div class="entry-content">';
-                  the_content();
-                  echo '</div>';
-                endwhile;
-              ?>
-            </div> 
+            <?php get_template_part('sidebar','right'); ?>
         </div>
 
 
--- sidebar-right.php
+++ sidebar-right.php
@@ -1,7 +1,26 @@
-<?php if (is_active_sidebar('sidebar-right')) { ?>
-                <p>sidebar right active</p>
-				<div class="col-md-3" id="sidebar-right">
-					<?php do_action('before_sidebar'); ?> 
-					<?php dynamic_sidebar('sidebar-right'); ?> 
-				</div>
-<?php } ?> 
+<h3>Upcoming Events</h3>
+<div class="content-box big ch-item bottom-pad-small">
+    <?php
+        $args = array( 'post_type' => 'news_item', 'posts_per_page' => 1 );
+        $loop = new WP_Query( $args );
+        while ( $loop->have_posts() ) : $loop->the_post();
+        the_title( '<h4>', '</h4>' );
+        echo '<div class="entry-content">';
+        the_content();
+        echo '</div>';
+        endwhile;
+    ?>    
+</div>
+<h3>Latest News</h3>
+<div class="content-box big ch-item bottom-pad-small">
+    <?php
+      $args = array( 'post_type' => 'business_item', 'posts_per_page' => 1 );
+      $loop = new WP_Query( $args );
+      while ( $loop->have_posts() ) : $loop->the_post();
+        the_title( '<h4>', '</h4>' );
+        echo '<div class="entry-content">';
+        the_content();
+        echo '</div>';
+      endwhile;
+    ?>    
+</div>
