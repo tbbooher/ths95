dd6ce361d1e2556936ec62db6cf89a1a252cfbac
Tim Booher <tim@theboohers.org>
2014-12-15 22:16:32 -0500

    upd

--- content-single.php
+++ content-single.php
@@ -1,6 +1,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<h1 class="entry-title">foo! <?php the_title(); ?></h1>
 
 		<div class="entry-meta">
 			<?php bootstrapBasicPostOn(); ?> 
