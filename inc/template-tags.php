dcdc9ecb7ad11b723b2add74eaaa2cde95170403
Tim Booher <tim@theboohers.org>
2014-12-16 05:49:51 -0500

    fixed function that was returning double

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -245,30 +245,30 @@ function bootstrapBasicPagination($pagination_align_class = 'pagination-center p
  */
 function bootstrapBasicPostOn() 
 {
-	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-	if (get_the_time('U') !== get_the_modified_time('U')) {
-		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
-	}
-
-	$time_string = sprintf($time_string,
-		esc_attr(get_the_date('c')),
-		esc_html(get_the_date()),
-		esc_attr(get_the_modified_date('c')),
-		esc_html(get_the_modified_date())
-	);
-
-	printf(__('<span class="posted-on">Posted on %1$s</span><span class="byline"> by %2$s</span>', 'bootstrap-basic'),
-		sprintf('<a href="%1$s" title="%2$s" rel="bookmark">%3$s</a>',
-			esc_url(get_permalink()),
-			esc_attr(get_the_time()),
-			$time_string
-		),
-		sprintf('<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
-			esc_url(get_author_posts_url(get_the_author_meta('ID'))),
-			esc_attr(sprintf(__('View all posts by %s', 'bootstrap-basic'), get_the_author())),
-			esc_html(get_the_author())
-		)
-	);
+//	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+//	if (get_the_time('U') !== get_the_modified_time('U')) {
+//		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
+//	}
+//
+//	$time_string = sprintf($time_string,
+//		esc_attr(get_the_date('c')),
+//		esc_html(get_the_date()),
+//		esc_attr(get_the_modified_date('c')),
+//		esc_html(get_the_modified_date())
+//	);
+//
+//	printf(__('<span class="posted-on">Posted on %1$s</span><span class="byline"> by %2$s</span>', 'bootstrap-basic'),
+//		sprintf('<a href="%1$s" title="%2$s" rel="bookmark">%3$s</a>',
+//			esc_url(get_permalink()),
+//			esc_attr(get_the_time()),
+//			$time_string
+//		),
+//		sprintf('<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
+//			esc_url(get_author_posts_url(get_the_author_meta('ID'))),
+//			esc_attr(sprintf(__('View all posts by %s', 'bootstrap-basic'), get_the_author())),
+//			esc_html(get_the_author())
+//		)
+//	);
 }// bootstrapBasicPostOn
 
 
